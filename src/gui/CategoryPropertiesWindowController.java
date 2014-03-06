2c24ff196d9f66ca8dbb7ec4788a966f5dd74f57
Josh Wright <joshuawright11@gmail.com>
2014-03-06 02:29:41 -0600

    Got color info, added empty title check.

--- src/gui/CategoryPropertiesWindowController.java
+++ src/gui/CategoryPropertiesWindowController.java
@@ -52,7 +52,6 @@ private Category category;
 
     @FXML // fx:id="renderColorLabel"
     private Label renderColorLabel; // Value injected by FXMLLoader
-    private Color color;
 
 
     // Handler for Button[fx:id="cancelButton"] onAction
@@ -67,46 +66,25 @@ private Category category;
     @FXML
     void createButtonPressed(ActionEvent event) {
         String title = categoryNameTextField.getText();
-        color = renderColorColorPicker.getValue();
-        System.out.println(color+" ");
+        Color color = renderColorColorPicker.getValue();
         Category c = new Category(title, color);
-        timelineMaker.getSelectedTimeline().addCategory(c);
-        timelineMaker.getSelectedTimeline().selectCategory(c.getName());
-        timelineMaker.populateView();
-        Node  source = (Node)  event.getSource(); 
-        Stage stage  = (Stage) source.getScene().getWindow();
-        stage.close();
+        if(!title.equals("")){
+	        timelineMaker.getSelectedTimeline().addCategory(c);
+	        timelineMaker.getSelectedTimeline().selectCategory(c.getName());
+	        timelineMaker.populateView();
+	        Node  source = (Node)  event.getSource(); 
+	        Stage stage  = (Stage) source.getScene().getWindow();
+	        stage.close();
+        }
         //TODO: Save the new category to the database.
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert buttonSeparator != null : "fx:id=\"buttonSeparator\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert categoryNameLabel != null : "fx:id=\"categoryNameLabel\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert categoryNameTextField != null : "fx:id=\"categoryNameTextField\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert createButton != null : "fx:id=\"createButton\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert renderColorColorPicker != null : "fx:id=\"renderColorColorPicker\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
-        assert renderColorLabel != null : "fx:id=\"renderColorLabel\" was not injected: check your FXML file 'CategoryPropertiesWindow.fxml'.";
+    	// nothing?
     }
     
     public void initData(TimelineMaker timelineMaker, Category category) {
-        //None of these methods on the color picker work!?
-        renderColorColorPicker = new ColorPicker();
-        renderColorColorPicker.setValue(Color.BEIGE);
-        renderColorColorPicker.setVisible(false);
-        if(category != null && category.getColor() != null){
-            color = category.getColor();
-            renderColorColorPicker.setValue(color);
-        }else color = Color.WHITE;
-        renderColorColorPicker.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event t) {
-                color = (renderColorColorPicker.getValue());    
-                System.out.println(""+renderColorColorPicker.getValue());
-            }
-        });
-
 	this.timelineMaker = timelineMaker;
 	this.category = category;
 	if(category != null){
